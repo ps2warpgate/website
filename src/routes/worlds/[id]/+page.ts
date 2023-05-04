ae73def9de8ca77cb22a5f254706792782563578
wupasscat <wupasscat10@gmail.com>
2023-05-04 11:30:12 -0400

    use cloudflare pages adaptor

--- src/routes/worlds/[id]/+page.ts
+++ src/routes/worlds/[id]/+page.ts
@@ -1,5 +1,5 @@
 export async function load({ fetch, params }) {
-    const response = fetch(`https://api.wupass.xyz/worlds/?id=${params.id}`)
+    const response = await fetch(`https://api.wupass.xyz/worlds/?id=${params.id}`)
     const world = await response.json()
     return world
 }
